969d2328e9e0bad4efb9bf5a6f9fd35d0b9cb8cc
Daniel Furman <djfurman@gmail.com>
2017-12-24 23:15:59 -0500

    Starting python testing experiments.

---
+++ features/steps/steps.py
@@ -0,0 +1,16 @@
+from behave import *
+from app import app
+
+@given('the application is running')
+def step_impl(context):
+    pass
+
+
+@when('we call the health check')
+def step_impl(context):
+    pass
+
+
+@then('the status code will be 200')
+def step_impl(context):
+    pass
